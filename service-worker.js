df68d23b8a3a9b66e529f4541928b1c2a4fdf84f
Onhacker <onhacker@MacBook-Pro-Onhacker.local>
2025-10-31 13:48:25 +0800

    first

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 /* ===== Service Worker ===== */
 
-const CACHE_NAME  = 'sila-60';                 // ⬅️ bump saat deploy
+const CACHE_NAME  = 'sila-61';                 // ⬅️ bump saat deploy
 const OFFLINE_URL = '/assets/offline.html';
 
 /* HTML publik yang boleh dicache (path tanpa query) */
