c40da5c32888158740af261c5d06dd71294f956c
Onhacker <onhacker@MacBook-Pro-Onhacker.local>
2025-11-03 12:03:55 +0800

    first

--- application/views/front_end/head.php
+++ application/views/front_end/head.php
@@ -4,7 +4,8 @@
   <!-- ========== META DASAR ========== -->
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, viewport-fit=cover">
+
   <meta name="robots" content="index, follow">
   <meta name="google" content="notranslate">
   <meta name="author" content="Onhacker.net">
