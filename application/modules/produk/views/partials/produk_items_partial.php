1e0db15dd96dd728688b808568c1973706e187dc
Onhacker <onhacker@MacBook-Pro-Onhacker.local>
2025-11-11 16:21:10 +0800

    first

--- application/modules/produk/views/partials/produk_items_partial.php
+++ application/modules/produk/views/partials/produk_items_partial.php
@@ -50,18 +50,9 @@
 
 
 <?php else: ?>
-<style type="text/css">
-        /* samakan basis gaya ribbon kamu */
-.corner-ribbon.trend { 
-  /* ambil style yang dulu kamu taruh di .corner-ribbon.hot */
-  background: #ff7a00; /* oranye = 'ngetren' */
-  color:#fff;
-}
-.corner-ribbon.bestseller {
-  background: #ffb300; /* emas = 'terlaris' */
-  color:#fff;
-}
-</style>
+
+
+
   <?php
     // ==== PARAM DISPLAY ====
     $BESTSELLER_MIN = 10;   // batas lifetime "Terlaris"
