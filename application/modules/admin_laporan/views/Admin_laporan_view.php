fcf627f111b00fc9209b2d3673bcee8b35197249
Onhacker <onhacker@MacBook-Pro-Onhacker.local>
2025-11-26 01:08:24 +0800

    first

--- application/modules/admin_laporan/views/Admin_laporan_view.php
+++ application/modules/admin_laporan/views/Admin_laporan_view.php
@@ -285,7 +285,7 @@
 
 <!-- Modal Analisa Bisnis (Gemini) -->
 <div class="modal fade" id="modalAnalisa" tabindex="-1" role="dialog" aria-labelledby="modalAnalisaTitle" aria-hidden="true">
-  <div class="modal-dialog modal-lg modal-dialog-scrollable" role="document">
+  <div class="modal-dialog  modal-full modal-lg modal-dialog-scrollable" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="modalAnalisaTitle">Analisa Bisnis AUSI</h5>
