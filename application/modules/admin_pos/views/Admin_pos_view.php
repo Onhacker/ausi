ec0aaf2c8fa29e125e9e1c8ca1cb49a9d9b9acfe
Onhacker <onhacker@MacBook-Pro-Onhacker.local>
2025-12-16 01:48:19 +0800

    first

--- application/modules/admin_pos/views/Admin_pos_view.php
+++ application/modules/admin_pos/views/Admin_pos_view.php
@@ -643,7 +643,7 @@ function loadJsOnce(src){
 window.openGmailInbox = async function(){
   try{
     await loadCssOnce("<?= base_url('assets/min/gmail.css?v=3') ?>");
-    await loadJsOnce("<?= base_url('assets/min/gmail.js?v=3') ?>");
+    await loadJsOnce("<?= base_url('assets/min/gmail.min.js?v=3') ?>");
     if (window.gmailInitAndOpen) window.gmailInitAndOpen(window.GMAIL_CFG);
     else $('#gmail-inbox-modal').modal('show');
   }catch(e){
