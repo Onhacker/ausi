d8127c03aa91ca1031ea04d5f5c3c5debec70ebb
Onhacker <onhacker@MacBook-Pro-Onhacker.local>
2025-12-17 21:37:19 +0800

    first

--- application/modules/admin_pos/controllers/Admin_pos.php
+++ application/modules/admin_pos/controllers/Admin_pos.php
@@ -625,8 +625,8 @@ return '<div class="d-flex flex-wrap" style="gap:.25rem .25rem"'
             if (!$isKitchen && !$isBar) {
                 $idInt = (int)$r->id;
 
-                $btnPaid   = '<button type="button" class="btn btn-xs btn-primary mr-1" onclick="mark_paid_one('.$idInt.')"><i class="mdi mdi-fingerprint"></i></button>';
-                $btnCancel = '<button type="button" class="btn btn-xs btn-secondary mr-1" onclick="mark_canceled_one('.$idInt.')" '.($isClosed ? 'disabled' : '').'><i class="fe-x-circle"></i></button>';
+                $btnPaid   = '<button type="button" class="btn btn-xs btn-primary mr-1" onclick="mark_paid_one('.$idInt.')"><i class="fe-dollar-sign"></i></button>';
+                $btnCancel = '<button type="button" class="btn btn-xs btn-secondary mr-1" onclick="mark_canceled_one('.$idInt.')" '.($isClosed ? 'disabled' : '').'><i class="fe-shield-off"></i></button>';
 
                 $unameLower = strtolower((string)$this->session->userdata('admin_username'));
                 $isAdmin    = ($unameLower === 'admin');
